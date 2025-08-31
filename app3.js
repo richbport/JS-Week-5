263d72c8c677cda01441312d75af4aeada2eb84f
Rich <richbport@yahoo.com>
2025-08-30 21:31:44 -0400

    exploreCave(cave)

--- app3.js
+++ app3.js
@@ -1,1 +1,26 @@
-console.log(123)
+const cave = {
+  name: "Entrance",
+  paths: [
+    {
+      name: "Left Tunnel",
+      paths: [
+        { name: "Small Chamber", paths: [] },
+        { name: "Deep Cave", paths: [{ name: "Hidden Room", paths: [] }] },
+      ],
+    },
+    {
+      name: "Right Tunnel",
+      paths: [{ name: "Crystal Cavern", paths: [] }],
+    },
+  ],
+};
+
+function exploreCave(cave) {
+  console.log("Exploring:", cave.name)
+
+  for (let path of cave.paths) {
+    exploreCave(path)
+  }
+}
+
+exploreCave(cave)
