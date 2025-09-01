06bc26bfa4911fbc74d2963a3593bbecc8933015
Rich <richbport@yahoo.com>
2025-09-01 10:56:04 -0400

    notes added to app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 function sum(n) {
   if (n === 1) {
-    return 1;
+    return 1; // Base case: Stop when n === 1
   }
-  return n + sum(n - 1);
+  return n + sum(n - 1); // Recursive case: n + the sum of the smaller numbers
 }
 
-console.log(sum(5));
+console.log(sum(5)); // Output: 15
